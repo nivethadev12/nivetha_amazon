0c17a461979e9376b9360cdb852d59079f0e6218
ayshanasreep <ayshanasreenp@gmail.com>
2024-01-29 10:37:24 +0530

    amzn

--- lib/ama.dart
+++ lib/ama.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:demo/meesho.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
